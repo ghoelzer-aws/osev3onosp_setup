13594c52a2d427254bf01c9245f7aab22a10efa2
Greg Hoelzer <ghoelzer@redhat.com>
2015-09-02 10:31:08 -0500

    Add new scripts

---
+++ mlbparks_webhook.sh
@@ -0,0 +1,4 @@
+curl -i -H "Accept: application/json" \
+-H "X-HTTP-Method-Override: PUT" -X POST -k \
+https://centralmw-osev3-mstr01.os1.phx2.redhat.com:8443/osapi/v1beta3/namespaces/joes-mlbparks/buildconfigs/openshift3mlbparks/webhooks/dRRttN5WTTgGFWowuJj2/generic
+
